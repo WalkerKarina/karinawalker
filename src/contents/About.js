657d468dfa2a2a654d168d21f2384d443b1a18bd
Karina Walker <karinawalker@Karinas-MacBook-Pro.local>
2020-12-20 13:23:29 +0900

    Evaluation

--- src/contents/About.js
+++ src/contents/About.js
@@ -3,9 +3,6 @@ import profilePicture from '../images/profile_image.png';
 import Footer from '../components/FooterComponent';
 import Social from '../components/SocialComponent';
 
-//get the screen size changing image movement to work
-//include hyperlinks to all my stuff in the about page
-
 class About extends Component {
 	render() {
 		return (
@@ -17,11 +14,11 @@ class About extends Component {
 						<h1>I'm Karina Walker</h1>
 						<br></br>
 						<p>
-							I'm Karina, class of 2025 at Harvey Mudd College, Claremont where I will be studying Mathematics and Computer Science. 
+							I'm Karina, class of 2025 at Harvey Mudd College, California, where I am set to study Mathematics and Computer Science. I am currently creating multiple web applications that incorporate machine learning solutions.  
 						</p>
 						<p>
-							During my 2020-2021 gap year, I have been studying Machine Learning and how to build products. I'm especially interested in the mathematics used in machine learning. 
-							Although I have been studying Computer Science since 2016, I found that the conventional subject of Computer Science taught me little about the actual process of how to  produce good products. As such, I've been working on full-stack web development as well as using tensorflow to integerate deep learning in various products.
+During my gap year from Aug 2020, amongst other certifications, I have completing the Deep Learing Specialization. I am especially interested in the mathematics used in machine learning and understanding lower level implementations.
+My current project is implementing OpenAI’s GPT2 text-generation model. As an online tutor, I spend a lot of time writing student reports.  As such, I aim to automate parts of the process. I initially attempted a conversion to tensorflow.js to run the model in browser, but found the model too large for the limited browser memory. Subsequently, I have progressed to creating a REST API to serve the model to browser.
 						</p>
 					</div>
 					<div className="col col-md-4 col-sm-8">
@@ -33,17 +30,17 @@ class About extends Component {
 					<div className="col col-md-8">
 						<h3>Current Projects</h3>
 						<ul>
-							<li><b>Teaching Report</b> — Transformed GPT2 NLP model into TensorFlow.js. Website which enables tutors and students to keep track of their online learning. </li>
-							<li><b>SOS For Haze</b> — Developed website for an NGO SOS For Haze. Raises awareness on the impact deforestation can have on the environment.</li>
-							<li><b>Doodle</b> — Implementation and creation of own CNN model using Doodle's Quick Draw Dataset. Identifies top 5 possible objects from sketch.</li>
-							<li><b>Online Whiteboard</b> — Developed website to enbale whiteboard drawings on the web.</li>
+							<li><b>Teaching Report</b> — Implementing GPT2 NLP model into browser to automate generation of reports. </li>
+							<li><b>SOS For Haze</b> — Developing a website for an NGO SOS For Haze. To raise awareness on the negative impact haze and deforestation has on the environment.</li>
 						</ul>
 						<br></br>
 						<h3>Past Projects</h3>
 						<ul>
+							<li><b>Doodle</b> — Implementation and creation of CNN model using Doodle's Quick Draw Dataset. Runs in brower using tensorflow.js</li>
 							<li><a href="http://shop.oogushi-farm2.co.jp/"><b>Oogushi Farm</b></a> — Re-created E-Commerce website selling fruits for a Japanese Farm. (Created both <a href="http://shop.oogushi-farm2.co.jp/">Oogushi Farm</a> and <a href="https://www.gogo-oogushi.com/">Oogushi Farm 2</a>) </li>
 							<li><a href="https://walkerkarina.github.io/my_portfolio_v2/#/about"><b>My Portfolio</b></a></li>
 							<li><b>Salude</b> — SheHacks U18 First Place Winner 2019. Created an application under the theme of "Empowerment". Presented to Visa Executives and various entrepreneurs.</li>
+							<li><b>Online Whiteboard</b> — Enbales whiteboard drawings on the web.</li>
 							<li><b>Easify NLP</b> — An automated recipe converter using spaCy, an NLP library. Automatic identification of key components in a recipe to enable units conversion. </li>
 						</ul>
 					</div>
--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -8,8 +8,8 @@ class Skills extends Component {
 	constructor(props) {
 		super(props);
 		this.state={
-			'myskills':['HTML','CSS','JS','PHP','REACT JS','MONGODB','REACT NATIVE'],
-			'myAwards':['SheHacks First Place U18 2019', 'UKMT Gold 2018 Maths', 'UKMT Silver 2019 Maths']
+			'myskills':['React.js', 'JavaScript', 'Python', 'node.js', 'Java', 'PHP','CSS/HTML','SQL','MongoDB'],
+			'myAwards':['SheHacks First Place U18 2019', 'Grade 11 Overall Academic Attainment Award (1 out of 3 from 360 students)', 'Grade 11 Mathematics Academic Award (1 out of 4 from 360 students)','UKMT Gold 2018 Maths Competition', 'UKMT Silver 2019 Maths Competition']
 		};
 	}
 
@@ -17,6 +17,22 @@ class Skills extends Component {
 	return (
 		<div className="container">
 			<div className="row-content">
+				<div className="col col-md-4">
+					<h1>My Certificates</h1>
+					<ul>
+						<li>DeepLearning.AI TensorFlow Developer Specialization (4 courses)</li>
+						<li>Mathematics for Machine Learning Specialization (3 courses)</li>
+						<li>Deep Learning Specialization (5 courses)</li>
+						<li>Python for Everybody Specialization (5 courses)</li>
+						<li>Building Database Applications in PHP</li>
+						<li>Front-End Web Development with React</li>
+						<li>Building Web Applications in PHP</li>
+						<li></li>
+						<li>Front-End Web UI Frameworks and Tools: Bootstrap 4</li>
+						<li>Introduction to Programming with MATLAB</li>
+						<li>Introduction to Mathematical Thinking</li>
+					</ul>
+				</div>
 				<div className="col col-md-4">
 					<h1>My Skills</h1>
 					<ul>
