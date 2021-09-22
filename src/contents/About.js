aa4616c587c27c6459ee740a5628b4fc060e4184
WalkerKarina <karina.walker.nicky@gmail.com>
2021-09-22 12:27:30 -0700

    update with Elinor

--- src/contents/About.js
+++ src/contents/About.js
@@ -16,15 +16,15 @@ class About extends Component {
 Hi, I'm Karina! 
 						</p>
 <p>
-I am a first-year at <a href="https://www.hmc.edu/">Harvey Mudd College</a> in California studying Mathematics and Computer Science. I’m half Japanese, half British and was born and raised in Singapore.						</p>
+I am a freshman at <a href="https://www.hmc.edu/">Harvey Mudd College</a> studying Mathematics and Computer Science. I’m half Japanese, half British, and was born and raised in Singapore.						</p>
 <p> 
-During my 2020-2021 gap year, I worked at <a href="https://www.coinhako.com/">Coinhako</a> as a Software Engineer for 3 months. I took the lead on a project to automate a user subscription service where I created an API and categorized subscribers to streamline operations. I also expanded a backend risk evaluation service and created a user-friendly front-end service to accompany it.
+During my 2020-2021 gap year, I worked at <a href="https://www.coinhako.com/">Coinhako</a> as a Software Engineer for three months. I was the project lead in automating a user subscription service. I created an API client and categorized subscribers to streamline operations. I also expanded a backend risk evaluation service and created a user-friendly front-end service to accompany it.
 </p>
 <p> 
-During my year, I also dived into the world of machine learning. I took various courses and created my own projects whilst unearthing the mathematical reasoning behind the computations. 
+During my gap year, I also dived into the world of machine learning. I took various courses and created my own projects to unearth the mathematical reasoning behind computations.
 </p>
 <p> 
-As an online tutor, I spend a lot of time writing student reports. So I'm currently working on applying some of the deep learning theories I've read about to automate this process.
+I also worked as a mathematics, social studies, and science tutor. I soon realized how time-intensive it is to write student reports for my six students. Subsequently, I have been working on applying some of the deep learning theories I have read about to automate this process.
 </p>
 
 
@@ -44,8 +44,8 @@ As an online tutor, I spend a lot of time writing student reports. So I'm curren
 						<h3>Past Projects</h3>
 						<ul>
 							<li><b><a href="https://walkerkarina.github.io/doodle/">Doodle</a></b> — Implementation and creation of CNN model using Doodle's Quick Draw Dataset. Runs in brower using tensorflow.js</li>
-							<li><a href="http://shop.oogushi-farm2.co.jp/"><b>Oogushi Farm</b></a> — Re-created E-Commerce website selling fruits for a Japanese Farm. (Created both <a href="http://shop.oogushi-farm2.co.jp/">Oogushi Farm</a> and <a href="https://www.gogo-oogushi.com/">Oogushi Farm 2</a>) </li>
-							<li><b>Salude</b> — SheHacks U18 First Place Winner 2019. Created an application under the theme of "Empowerment". Presented to Visa Executives and various entrepreneurs.</li>
+							<li><a href="http://shop.oogushi-farm2.co.jp/"><b>Oogushi Farm</b></a> — Re-created E-Commerce website selling fruits for a Japanese Farm. I created both <a href="http://shop.oogushi-farm2.co.jp/">Oogushi Farm</a> and <a href="https://www.gogo-oogushi.com/">Oogushi Farm 2</a>) </li>
+							<li><b>Salude</b> — SheHacks U18 First Place Winner 2019. I designed an application under the theme of "Empowerment" and presented my application to Visa Executives and various entrepreneurs.</li>
 						</ul>
 					</div>
 				</div>
--- src/contents/Interests.js
+++ src/contents/Interests.js
@@ -17,16 +17,18 @@ class Interests extends Component {
 		<div className="row row-content">
 				<div className="col-12 col-md-8">
 				 	<h4>Some of my Gap Year experiences</h4>
+				 	<p>My main goal for my gap year was to experience as many different and exciting things the world has to offer as possible. This included:</p>
 				 	<ul>
-				 		<li>Became a skateboarder — became a regular at the skateparks in Singapore</li>
-				 		<li>Worked at <a href="https://rusutsu.com/en/">Rusutsu</a> ski resort in Hokkaido for 3 months — progressed from a beginner to advanced snowboarder</li>
-				 		<li>Executed a solo expedition around northern Hokkaido</li>
-				 		<li>Surfed in coastal Japan — I can stand!</li>
-				 		<li>Paraglided in Kyoto</li>
-				 		<li>Travelled historical sights in Osaka and Kyoto</li>
-				 		<li>Volunteered at an apple farm — <a href="https://www.youtube.com/watch?v=thGnQ6msE78&t=74s">pick apples with me</a>, <a href="https://www.youtube.com/watch?v=k_v5xKGXWWw&t=165s">walk around the farm with me</a>, and a <a href="https://www.instagram.com/p/CICq4aEjfLo/">promotional video I created</a></li>
-				 		<li>Completed a Manual Driver's License camp</li>
-				 		<li>Software Engineer internship at a cryptocurrency brokerage firm</li>
+				 		<li>A Software Engineer internship at a cryptocurrency brokerage firm</li>
+				 		<li>Learning how to skateboard. I soon became a regular at the skateparks in Singapore!</li>
+				 		<li>Worked at <a href="https://rusutsu.com/en/">Rusutsu</a> ski resort in Hokkaido, Japan for 3 months. Now, I cannot stop dreaming about the next time I will get to go snowboarding!</li>
+				 		<li>Paragliding in Kyoto, Japan. </li>
+				 		<li>Executing a solo expedition around northern Hokkaido, Japan. I swam in arctic drift ice, visited an ice village, and led a self-guided local food tour. </li>
+				 		<li>Surfing in coastal Japan. (I can now stand!)</li>
+				 		<li>Backpacking around Osaka and Kyoto. Having grown up outside of my home country, Japan, this was especially meaningful to me as I got to reconnect with my roots. </li>
+				 		<li>Experimenting with vegan and vegetarian cooking. I even convinced my parents to try my adventurous recipes of colorful beans and ancient grains! </li>
+				 		<li>Getting my Manual Driver's License.</li>
+				 		<li>Volunteering at an apple farm that aims to revolutionize traditional Japanese farming with technology. I created <a href="https://www.instagram.com/p/CICq4aEjfLo/">social media content</a> to promote the farm. Click on these links to <a href="https://www.youtube.com/watch?v=thGnQ6msE78&t=74s">pick apples with me</a> and <a href="https://www.youtube.com/watch?v=k_v5xKGXWWw&t=165s">walk around the farm with me.</a></li>
 				 	</ul>
 				</div>
 				<div className="col-12 col-md-4">
@@ -40,21 +42,17 @@ class Interests extends Component {
 				<div className="col col-md-12">
 					<h4>Ethical Eating</h4>
 					<p>
-When I turned 18, I became a vegetarian. I’ve spent the past two years plunging into the ethics of environmentally friendly eating. It’s deeply important to me that I view this change as a choice. So for every meat product I’ve chosen not to consume, I’ve found 2-3 new foods to taste and incorporate into my diet.
-						</p>
-					<p>
-During my gap year, I focused on exploring nutritional science and veganism. I tasted a rainbow of beans and ancient grains and convinced my parents to entertain my adventurous recipes.
-
+When I turned 18, I became a vegetarian. I have spent the past two years plunging into the ethics of environmentally friendly eating and nutritional science. It is deeply important to me that I view this change as a choice. As such, for every meat product I have chosen not to consume, I have found 2-3 new foods to taste and incorporate into my diet. 
 					</p>
 					<p>
-I took it upon myself to become the designated restaurant finder amongst my friends and even surprised them with elusively meaty but 100% veggie food. Over the past few months, I’ve warmly welcomed two of my friends into the world of vegetarianism.
+I have now become the designated restaurant finder for my friends and have continuously surprised them with elusively meaty but 100% veggie food. Over the past few months, I have warmly welcomed two of my friends into the world of vegetarianism.
 					</p>
 				</div>
 			</div>
 			<div className="row row-content">
 				<div className="col col-md-12">
-				 	<h4>I’m really into documentaries and historical dramas. Here are some of my favourites:
-</h4>
+				 	<h4>I also love documentaries and historical dramas. </h4>
+				 	<p>Here are some of my favorites:</p>
 				 	<ul>
 				 		<li>The Great</li>
 				 		<li>Planet of The Humans</li>
