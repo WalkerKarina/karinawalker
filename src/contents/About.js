31f2cd41d9a47d7399eca0dc3180e570dc66d33a
Karina Walker <42312377+WalkerKarina@users.noreply.github.com>
2022-06-28 15:08:11 -0400

    three not 3

--- src/contents/About.js
+++ src/contents/About.js
@@ -16,7 +16,7 @@ Hi, I'm Karina!
 						</p>
 <p>
 I am a sophomore at <a href="https://www.hmc.edu/">Harvey Mudd College</a> studying Mathematics and Computer Science. I’m half Japanese, half British, and was born and raised in Singapore.						</p>
-<p>In the summer of 2022, I joined the PayPal Braintree SDK team as a Software Engineer for 3 months. I created a Slackbot to automate and streamline service processes. Under DevOps, I also peer programmed for maintenance and evaluated the automation of SDKs.
+<p>In the summer of 2022, I joined the PayPal Braintree SDK team as a Software Engineer for three months. I created a Slackbot to automate and streamline service processes. Under DevOps, I also peer programmed for maintenance and evaluated the automation of SDKs.
 </p>
 <p> 
 During my 2020-2021 gap year, I worked at <a href="https://www.coinhako.com/">Coinhako</a> as a Software Engineer for three months. I was the project lead in automating a user subscription service. I created an API client and categorized subscribers to streamline operations. I also expanded a backend risk evaluation service and created a user-friendly front-end service to accompany it.
